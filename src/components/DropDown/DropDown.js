d7b3bd7e80f4895e1f27b4ab215eb77f406528ff
Kevin <tig3r86@gmail.com>
2016-01-14 12:07:41 +1030

    add fixed to make fixed navigation menuBar behaves as required

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import sty from './DropDown.scss'
 import FaAngleDown from 'react-icons/lib/fa/angle-down'
 import FaAngleUp from 'react-icons/lib/fa/angle-up'
@@ -7,44 +7,39 @@ import classNames from 'classnames/bind'
 
 let cx = classNames.bind(sty)
 
-export default class DropDown extends Component {
-  static propTypes = {
-    dropDownLinks: PropTypes.array.isRequired,
-    dropDownActivate: React.PropTypes.func.isRequired,
-    dropDownStatus: React.PropTypes.bool.isRequired
-  }
-
-  render() {
-    const { dropDownLinks, dropDownActivate, dropDownStatus } = this.props
-    let dropDown = cx({
-      dropDown: dropDownStatus === false,
-      dropDownOpen: dropDownStatus === true
-    })
-    let containerDropDown = cx({
-      container: dropDownStatus === false,
-      containerDropDown: dropDownStatus === true
-    })
-
-    return (
-      <div className={containerDropDown}>
-        <div className={sty.header} onClick={dropDownActivate}>
-          <p>Portfolio</p>
-          {dropDownStatus === false ? <FaAngleDown /> : <FaAngleUp />}
-        </div>
-        <div className={dropDown}>
-          {dropDownLinks.map((link, index) =>
-            <Link
-              to={`/${link}`}
-              key={index}
-              onClick={dropDownActivate}
-              activeClassName={sty.activeLink}
-            >
-              {link}
-          </Link>
-          )}
-        </div>
+export default function DropDown ({dropDownLinks, dropDownActivate, dropDownStatus}) {
+  let dropDown = cx({
+    dropDown: dropDownStatus === false,
+    dropDownOpen: dropDownStatus === true
+  })
+  let containerDropDown = cx({
+    container: dropDownStatus === false,
+    containerDropDown: dropDownStatus === true
+  })
+  return (
+    <div className={containerDropDown}>
+      <div className={sty.header} onClick={dropDownActivate}>
+        <p>Portfolio</p>
+        {dropDownStatus === false ? <FaAngleDown /> : <FaAngleUp />}
       </div>
-    )
-  }
+      <div className={dropDown}>
+        {dropDownLinks.map((link, index) =>
+          <Link
+            to={`/${link}`}
+            key={index}
+            onClick={dropDownActivate}
+            activeClassName={sty.activeLink}
+          >
+            {link}
+        </Link>
+        )}
+      </div>
+    </div>
+  )
 }
 
+DropDown.propTypes = {
+  dropDownLinks: PropTypes.array.isRequired,
+  dropDownActivate: PropTypes.func.isRequired,
+  dropDownStatus: PropTypes.bool.isRequired
+}
--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -1,5 +1,6 @@
 import '../styles/core.scss'
 import Navigation from '../containers/Navigation/Navigation.js'
+import sty from './CoreLayout.scss'
 // Note: Stateless/function components *will not* hot reload!
 // react-transform *only* works on component classes.
 //
@@ -11,9 +12,9 @@ import Navigation from '../containers/Navigation/Navigation.js'
 // define it with a plain javascript function...
 function CoreLayout ({ children }) {
   return (
-    <div className='page-container'>
+    <div className='pageContainer'>
       <Navigation />
-      <div className='view-container'>
+      <div className={sty.viewContainer}>
         {children}
       </div>
     </div>
