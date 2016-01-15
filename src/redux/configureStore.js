64300e9984ffd7a9321ca2addb5de96bf6788f96
Kevin <tig3r86@gmail.com>
2016-01-15 12:05:12 +1030

    fix sticky menubar style on ios browser, and turn off redux devtool

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -10,16 +10,17 @@ export default function configureStore (initialState) {
   let createStoreWithMiddleware
   const middleware = applyMiddleware(thunk)
 
-  if (__DEBUG__) {
-    createStoreWithMiddleware = compose(
-      middleware,
-      window.devToolsExtension
-        ? window.devToolsExtension()
-        : require('containers/DevTools').default.instrument()
-    )
-  } else {
-    createStoreWithMiddleware = compose(middleware)
-  }
+  // turning this devtool off when using browsersync for multiple devices css style development. turn back on when you need to play around redux state
+  // if (__DEBUG__) {
+  //   createStoreWithMiddleware = compose(
+  //     middleware,
+  //     window.devToolsExtension
+  //       ? window.devToolsExtension()
+  //       : require('containers/DevTools').default.instrument()
+  //   )
+  // } else {
+  createStoreWithMiddleware = compose(middleware)
+  // }
 
   const store = createStoreWithMiddleware(createStore)(
     rootReducer, initialState
