40f941f21c7f13cb91bd894dff0746e4d2e1b357
Kevin <tig3r86@gmail.com>
2016-01-23 07:28:39 +1030

    add hacktimer script to stop chrome from throttling the setInterval timer to make the timer accurate even when the tab is out of focus in browser

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// this is an overide to the setInterval method to stop chrome from slowing down the call to interval when the user navigate to another tab. the script basically use webworkers api to run the setInterval from my brief look at the source
+//
 // change back to createBrowserHistory when you want to host on your own server
 // import createBrowserHistory from 'history/lib/createBrowserHistory'
 // import createHashHistory from 'history/lib/createHashHistory'
@@ -6,6 +8,9 @@ import { syncReduxAndRouter } from 'redux-simple-router'
 import routes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
+
+// refer here for the source https://github.com/turuslan/HackTimer
+import 'hacktimer'
 // import useScroll from 'scroll-behavior/lib/useStandardScroll'
 
 // const history = createBrowserHistory()
