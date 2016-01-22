480df5be8af92a69bae04419c8fdf4b09e7db3dd
Kevin <tig3r86@gmail.com>
2016-01-22 14:08:44 +1030

    modify css and scss loader in webpack to allow global import without use of css module from node-modules

--- build/webpack-environments/_base.js
+++ build/webpack-environments/_base.js
@@ -86,6 +86,7 @@ const webpackConfig = {
       },
       {
         test: /\.scss$/,
+        include: paths.client(),
         loaders: [
           'style',
           CSS_LOADER,
@@ -93,12 +94,33 @@ const webpackConfig = {
           'sass'
         ]
       },
+      // global scss import from node module without use of css-module
       {
-        test: /\.css$/,
+        test: /\.scss$/,
+        include: paths.base('node_modules'),
         loaders: [
           'style',
           'css?sourceMap',
-          // CSS_LOADER,
+          'postcss',
+          'sass'
+        ]
+      },
+      {
+        test: /\.css$/,
+        include: paths.client(),
+        loaders: [
+          'style',
+          CSS_LOADER,
+          'postcss'
+        ]
+      },
+      // global css import from node module without use of css-module
+      {
+        test: /\.css$/,
+        include: paths.base('node_modules'),
+        loaders: [
+          'style',
+          'css?sourceMap',
           'postcss'
         ]
       },
