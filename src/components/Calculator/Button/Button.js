988542119b8862c565bd262eb5d1ed89a4b0cb0d
Kevin <tig3r86@gmail.com>
2016-01-24 10:41:18 +1030

    modify Button component to take extra background props

--- src/components/Calculator/Button/Button.js
+++ src/components/Calculator/Button/Button.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react'
 import sty from './Button.scss'
 
-export default function Button ({value}) {
+export default function Button ({value, background}) {
+  let style = {
+    background: `${background}`
+  }
   return (
-    <div className={sty.container}>
+    <div className={sty.container} style={style}>
       <span>{value}</span>
     </div>
   )
