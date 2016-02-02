135dffcf3f010017b9fef86d3492a4c6001fe0cb
Kevin <tig3r86@gmail.com>
2016-02-02 14:21:00 +1030

    add logic for getting user location

--- src/redux/modules/Weather/Weather.js
+++ src/redux/modules/Weather/Weather.js
@@ -4,16 +4,22 @@ import { createAction, handleActions } from 'redux-actions'
  * Constants
  */
 
-export const SIDEBAR_ACTIVATE = 'SIDEBAR_ACTIVATE'
+export const LOCATION_REQUEST = 'LOCATION_REQUEST'
+export const LOCATION_RECEIVE = 'LOCATION_RECEIVE'
+export const LOCATION_ERROR = 'LOCATION_ERROR'
 
 /**
  * Actions
  */
 
-export const sidebarActivate = createAction(SIDEBAR_ACTIVATE)
+export const locationReceive = createAction(LOCATION_RECEIVE, location => location)
+export const locationRequest = createAction(LOCATION_REQUEST, status => status)
+export const locationError = createAction(LOCATION_ERROR, error => error)
 
 export const actions = {
-  sidebarActivate
+  locationRequest,
+  locationReceive,
+  locationError
 }
 
 /**
@@ -21,12 +27,40 @@ export const actions = {
  */
 
 const initialState = {
-  location: 'blah'
+  location: {
+    longitude: '',
+    latitude: '',
+    requestStatus: false,
+    error: {
+      status: false,
+      message: ''
+    }
+  }
 }
 
 export const Weather = handleActions({
-  SIDEBAR_ACTIVATE: (state) => Object.assign({}, state, {
-    sideBarStatus: !state.sideBarStatus
+  LOCATION_RECEIVE: (state, {payload}) => Object.assign({}, state, {
+    location: {...state.location,
+      longitude: payload.longitude,
+      latitude: payload.latitude
+    }
+  }),
+
+  LOCATION_REQUEST: (state, {payload}) => Object.assign({}, state, {
+    location: {...state.location,
+      requestStatus: payload
+    }
+  }),
+
+  LOCATION_ERROR: (state, {payload}) => Object.assign({}, state, {
+    location: {...state.location,
+      error: {
+        ...state.location.error,
+        status: true,
+        message: payload.message,
+        code: payload.code
+      }
+    }
   })
 }, initialState)
 
--- src/views/Weather/Weather.js
+++ src/views/Weather/Weather.js
@@ -9,15 +9,48 @@ const mapStateToProps = (state) => ({
 
 export default class Weather extends Component {
   static propTypes = {
-    location: PropTypes.string
+    location: PropTypes.object,
+    locationReceive: PropTypes.func.isRequired,
+    locationRequest: PropTypes.func.isRequired,
+    locationError: PropTypes.func.isRequired
   };
+
   render() {
     const { location } = this.props
     return (
       <div className={sty.container}>
-        {location}
+        {location.longitude}
+        {location.latitude}
+        {location.error.message}
       </div>
     )
+  }
+
+  componentDidMount() {
+    const { locationReceive, locationRequest, locationError } = this.props
+    // if geolocation is available
+    if ("geolocation" in window.navigator) {
+      locationRequest(true)
+      const geoSuccess = (position) => {
+        locationReceive({latitude: position.coords.latitude, longitude: position.coords.longitude})
+        locationRequest(false)
+      }
+      const geoError = (error) => {
+        console.log(error)
+        locationError(error)
+      }
+      const geoOptions = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+      }
+
+      window.navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions)
+
+    } else {
+      window.alert("This browser does not support geolocation")
+      return
+    }
   }
 }
 
