146ccf649371be6f583d97afab9c9c2af0847075
Kevin <tig3r86@gmail.com>
2016-02-18 12:08:19 +1030

    add individual card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,23 @@
 import React, { PropTypes } from 'react'
+import { isEmpty } from 'lodash'
 import sty from './Card.scss'
 
 export default function Card ({item}) {
   return (
     <div className={sty.container}>
-
+      {isEmpty(item.thumbnail)
+        ? <div className={sty.noImage}></div>
+        : <div className={sty.image} style={{backgroundImage: `url('${item.thumbnail.source}')`}}>
+          </div>
+      }
+      <div className={sty.content}>
+        <div className={sty.header}>
+          <h3>{item.title}</h3>
+        </div>
+        <div className={sty.text}>
+          <p>{item.extract}</p>
+        </div>
+      </div>
     </div>
   )
 }
