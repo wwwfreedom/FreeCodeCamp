4de8cd0c227ed043e2dac82a7b029fc249b0b291
Kevin <tig3r86@gmail.com>
2016-02-22 06:51:39 +1030

    add grammar fix to portfolio description and change picture in about component

--- src/views/Portfolio/Portfolio.js
+++ src/views/Portfolio/Portfolio.js
@@ -5,37 +5,37 @@ import CardV2 from 'components/CardV2/CardV2'
 const projects = [
   {
     name: 'Random Funny Quote',
-    description: 'A simple quote generator that you can tweet and share',
+    description: 'A simple quote generator that you can tweet and share.',
     image: 'http://i.imgur.com/NTVKdeT.png',
     link: 'quoteGenerator'
   },
   {
     name: 'Weather App',
-    description: 'A simple app to locate your current position and tell you the current weather',
+    description: 'A simple app to locate your current position and tell you the current weather.',
     image: 'http://i.imgur.com/gpOj6rf.png',
     link: 'weather'
   },
   {
     name: 'Simple Wikipedia Search',
-    description: 'A useful app to search wikipedia plain English articles',
+    description: "A useful app to search Wikipedia's plain English articles.",
     image: 'http://i.imgur.com/yVYtLa3.png',
     link: 'wikipedia'
   },
   {
     name: 'Twitch Streamers',
-    description: 'A simple widget to show online status of Twitch streamers',
+    description: 'A simple widget to show online status of Twitch streamers.',
     image: 'http://i.imgur.com/14LOigT.png',
     link: 'twitch'
   },
   {
     name: 'Calculator',
-    description: 'A simple calculator with ability to chain operations',
+    description: 'A simple calculator with the ability to chain operations.',
     image: 'http://i.imgur.com/GUR45JT.png',
     link: 'calculator'
   },
   {
     name: 'PomoTime',
-    description: 'Pomodoro timer featuring notification and audible alarm',
+    description: 'Pomodoro timer featuring notification and audible alarm.',
     image: 'http://i.imgur.com/vvEZbX8.png',
     link: 'pomoTime'
   }
