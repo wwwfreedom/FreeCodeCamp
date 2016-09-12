32a7d63337d95487d6f415ee81af08f2c0bd64ae
Kevin <tig3r86@gmail.com>
2016-09-12 18:38:53 +0930

    add copyright and change styling of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,11 @@ import sty from './Footer.scss'
 export default function Footer () {
   return (
     <div className={sty.container}>
-      <p>Built using React.js. Check out the source <a href="https://github.com/wwwfreedom/FreeCodeCamp" target='_blank'>here</a></p>
+      <p>
+        Built using React.js. Check out the source
+        <a href="https://github.com/wwwfreedom/FreeCodeCamp" target='_blank'> here. </a>
+         &copy; {new Date().getFullYear()}
+      </p>
     </div>
   )
 }
