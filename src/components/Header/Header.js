e68611529b80eb6afadf2d4f5514ceb34d479cd5
Kevin <tig3r86@gmail.com>
2016-09-12 18:40:04 +0930

    change styling of Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes} from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import {Link} from 'react-router'
 import sty from './Header.scss'
 
 export default class Header extends Component {
@@ -10,31 +12,22 @@ export default class Header extends Component {
   };
 
   render() {
-    const { headerImage, headerText, headerParagraph, separator } = this.props
-    let style = {
-      backgroundImage: `url('${headerImage}')`
-    }
     return (
       <div className={sty.container}>
-        <div className={sty.bgImage} style={style}>
-          <div className={sty.content}>
-            <h1>{headerText}</h1>
-            {this.renderSeparator(separator)}
-            {headerParagraph ? <p>{headerParagraph}</p> : ''}
-          </div>
+        <div className={sty.content}>
+          <h1 style={{display: 'none'}}>Kevin Truong</h1>
+          <h2>Ideas, Code, Deploy <br/> Specializing in responsive <br/> interactive web apps</h2>
+          <h3>Full-stack Javascript Developer</h3>
         </div>
-        <div className={sty.bgPattern}></div>
+        <div className={sty.cta}>
+          <Link to='/about'>
+            <RaisedButton label='What I offer'></RaisedButton>
+          </Link>
+          <Link to='/portfolio'>
+            <RaisedButton label='My work experience'></RaisedButton>
+          </Link>
+        </div>
       </div>
     )
   }
-
-  renderSeparator = (separator) => {
-    if (separator === true) {
-      return (
-        <div className={sty.separator}>
-          <div className={sty.lineSeparator}>∎</div>
-        </div>
-      )
-    }
-  };
 }
