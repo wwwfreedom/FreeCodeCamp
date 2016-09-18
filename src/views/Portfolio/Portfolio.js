7c115360195d4593ffab80e794aaf5e22f5e5c3b
Kevin <tig3r86@gmail.com>
2016-09-19 08:57:18 +0930

    change imgur images links to https

--- src/views/Portfolio/Portfolio.js
+++ src/views/Portfolio/Portfolio.js
@@ -6,56 +6,56 @@ const projects = [
   {
     name: 'PollWise',
     description: 'A full stack polling app where registered user can create their own polls and share it.',
-    image: 'http://i.imgur.com/8zcWJpJ.png',
+    image: 'https://i.imgur.com/8zcWJpJ.png',
     link: 'https://pollwise.herokuapp.com/',
     externalLink: 'true'
   },
   {
     name: 'Simple Wikipedia',
     description: "A useful app to search Wikipedia's plain English articles.",
-    image: 'http://i.imgur.com/yVYtLa3.png',
+    image: 'https://i.imgur.com/yVYtLa3.png',
     link: 'wikipedia'
   },
   {
     name: 'Twitch Streamers',
     description: 'A simple widget to show online status of Twitch streamers.',
-    image: 'http://i.imgur.com/14LOigT.png',
+    image: 'https://i.imgur.com/14LOigT.png',
     link: 'twitch'
   },
   {
     name: 'Calculator',
     description: 'A simple calculator with the ability to chain operations.',
-    image: 'http://i.imgur.com/GUR45JT.png',
+    image: 'https://i.imgur.com/GUR45JT.png',
     link: 'calculator'
   },
   {
     name: 'PomoTime',
     description: 'Pomodoro timer featuring notification and audible alarm.',
-    image: 'http://i.imgur.com/vvEZbX8.png',
+    image: 'https://i.imgur.com/vvEZbX8.png',
     link: 'pomoTime'
   },
   {
     name: 'Tic Tac Toe',
     description: 'A simple Tic Tac Toe game featuring simple artificial intelligence',
-    image: 'http://i.imgur.com/WStyuor.png',
+    image: 'https://i.imgur.com/WStyuor.png',
     link: 'TicTacToe'
   },
   {
     name: 'Simon Game',
     description: 'A simple Simon Game',
-    image: 'http://i.imgur.com/kgz9g7o.png',
+    image: 'https://i.imgur.com/kgz9g7o.png',
     link: 'SimonGame'
   },
   {
     name: 'Weather App',
     description: 'Weather app that locate your current position and tell you the current weather.',
-    image: 'http://i.imgur.com/gpOj6rf.png',
+    image: 'https://i.imgur.com/gpOj6rf.png',
     link: 'weather'
   },
   {
     name: 'Funny Quote Generator',
     description: 'Quote generator app that you can share the quotes via twitter.',
-    image: 'http://i.imgur.com/NTVKdeT.png',
+    image: 'https://i.imgur.com/NTVKdeT.png',
     link: 'quoteGenerator'
   }
 ]
